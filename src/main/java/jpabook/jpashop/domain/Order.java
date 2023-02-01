b525ac0b0104987a50353b2d6e31580b43924bb3
hayoun <fishingest@kookmin.ac.kr>
2023-02-02 01:05:30 +0900

    Order 엔티티 생성

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="orders")
+@Getter @Setter
+public class Order {
+    @Id @GeneratedValue
+    @Column(name="order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    private List<OrderItem> orderItems=new ArrayList<OrderItem>();
+
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    private OrderStatus status; // 주문상태 ORDER,CANCEL
+}
