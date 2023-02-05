6f786ef435f8f03ea1cec4ba326a166dc80ed838
hayoun <fishingest@kookmin.ac.kr>
2023-02-06 01:28:57 +0900

    엔티티 수정

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -16,14 +16,16 @@ public class Order {
     @Column(name="order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="member_id")
     private Member member;
 
-    @OneToMany(mappedBy ="order")
+    @OneToMany(mappedBy ="order",cascade = CascadeType.ALL)
+    //Order를 저장하면, orderItems도 같이 저장하게 되는 기능
     private List<OrderItem> orderItems=new ArrayList<OrderItem>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    //order를 persist해주면, delivery도 persist 해준다.
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -31,4 +33,23 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 ORDER,CANCEL
+
+    //연간관계 편의 메서드(양방향 관계 일때 해당한다)
+
+    public void setMember(Member member){
+        this.member=member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery=delivery;
+        delivery.setOrder(this);
+    }
+
+    // 이렇게 연관관계 메서드는 실질적으로 컨트롤하는쪽에서 다뤄주는게 좋다
 }
